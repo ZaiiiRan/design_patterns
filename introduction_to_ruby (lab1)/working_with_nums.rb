b72bce346b7ccc0c766682ac8e60b911c03e383d
ZaiiiRan <a1b075753@gmail.com>
2024-09-07 20:24:23 +0300

    product of digits not divisible by 5

--- introduction_to_ruby (lab1)/working_with_nums.rb
+++ introduction_to_ruby (lab1)/working_with_nums.rb
@@ -31,7 +31,37 @@ def max_prime_divider(number)
         return number
     end
     return max_divider
-
 end
 
-puts max_prime_divider(26)
+
+
+def product_of_digits_not_divisible_by_5(number)
+    number = number.abs
+    res = 1
+    has_not_5_or_0 = false
+
+    while number > 0 
+        digit = number % 10
+        number /= 10
+
+        if digit == 1
+            has_not_5 = true
+            next
+        elsif digit % 5 != 0
+            has_not_5_or_0 = true
+            res *= digit
+        end
+    end
+
+    if has_not_5_or_0 then
+        return res
+    else
+        return 0
+    end
+end
+
+
+
+
+puts "Max prime divider: #{max_prime_divider(26)}"
+puts "Product of digits not divisible by 5: #{product_of_digits_not_divisible_by_5(2535)}"
