a4921516f697e7707e8478eb9d6ac559e5707435
ZaiiiRan <a1b075753@gmail.com>
2024-09-14 18:02:38 +0300

    set contacts method

--- Kernel_Classes_and_Models (lab2)/student.rb
+++ Kernel_Classes_and_Models (lab2)/student.rb
@@ -1,5 +1,5 @@
 class Student
-    attr_reader :first_name, :name, :patronymic, :telegram, :email, :git
+    attr_reader :first_name, :name, :patronymic, :telegram, :email, :git, :phone_number
     attr_accessor :id
 
     # constructor
@@ -8,67 +8,7 @@ class Student
         self.first_name = first_name
         self.name = name
         self.patronymic = patronymic
-        self.phone_number = params[:phone_number]
-        self.telegram = params[:telegram]
-        self.email = params[:email]
-        self.git = params[:git]
-    end
-
-    # phone number validation
-    def self.valid_phone_number?(phone_number)
-        phone_number.nil? || phone_number =~ /^(?:\+7|8)[\s-]?(?:\(?\d{3}\)?[\s-]?)\d{3}[\s-]?\d{2}[\s-]?\d{2}$/
-    end
-
-    # telegram validation
-    def self.valid_telegram?(telegram)
-        telegram.nil? || telegram =~ /@[a-zA-Z0-9_]{5,}$/
-    end
-
-    # git link validation
-    def self.valid_git?(git)
-        git.nil? || git =~ %r{^https?://github\.com/[a-zA-Z0-9_\-]+$}
-    end
-
-    # email validation
-    def self.valid_email?(email)
-        email.nil? || email =~ /^[\w+_.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-    end
-
-    # names validation
-    def self.valid_name?(name)
-        name =~ /^[А-ЯЁ][а-яё]{1,}(-[А-ЯЁ][а-яё]{1,})?$/
-    end
-
-    # phone number setter
-    def phone_number=(phone_number)
-        unless self.class.valid_phone_number?(phone_number)
-            raise ArgumentError, "Wrong phone number format"
-        end
-        @phone_number = phone_number
-    end
-
-    # telegram setter
-    def telegram=(telegram)
-        unless self.class.valid_telegram?(telegram)
-            raise ArgumentError, "Wrong telegram format"
-        end
-        @telegram = telegram
-    end
-
-    # git setter
-    def git=(git)
-        unless self.class.valid_git?(git)
-            raise ArgumentError, "Wrong git link format"
-        end
-        @git = git
-    end
-
-    #email setter
-    def email=(email)
-        unless self.class.valid_email?(email)
-            raise ArgumentError, "Wrong Email format"
-        end
-        @email = email
+        self.set_contacts(git: params[:git], email: params[:email], telegram: params[:telegram], phone_number: params[:phone_number])
     end
 
     # first name setter
@@ -95,6 +35,74 @@ class Student
         @patronymic = patronymic
     end
 
+    # contacts setter
+    def set_contacts(contacts)
+        validate(contacts[:git], contacts[:phone_number], contacts[:telegram], contacts[:email])
+
+        unless self.class.valid_phone_number?(contacts[:phone_number])
+            raise ArgumentError, "Wrong phone number format"
+        end
+        @phone_number = contacts[:phone_number]
+
+        unless self.class.valid_telegram?(contacts[:telegram])
+            raise ArgumentError, "Wrong telegram format"
+        end
+        @telegram = contacts[:telegram]
+
+        unless self.class.valid_git?(contacts[:git])
+            raise ArgumentError, "Wrong git link format"
+        end
+        @git = contacts[:git]
+
+        unless self.class.valid_email?(contacts[:email])
+            raise ArgumentError, "Wrong Email format"
+        end
+        @email = contacts[:email]
+    end
+
+    # print info
+    def print_info
+        puts '-------------------'
+
+        puts "ID: #{ @id ? @id : 'Not Set' }"
+        puts "First Name: #{ @first_name }"
+        puts "Name: #{ @name }"
+        puts "Patronymic: #{ @patronymic }"
+        puts "Phone Number: #{ @phone_number ? @phone_number : 'Not Set' }"
+        puts "Telegram: #{ @telegram ? @telegram : 'Not Set' }"
+        puts "Email: #{ @email ? @email : 'Not Set' }"
+        puts "Git: #{ @git ? @git : 'Not Set' }"
+
+        puts '-------------------'
+    end
+
+
+    private
+
+    # phone number validation
+    def self.valid_phone_number?(phone_number)
+        phone_number.nil? || phone_number =~ /^(?:\+7|8)[\s-]?(?:\(?\d{3}\)?[\s-]?)\d{3}[\s-]?\d{2}[\s-]?\d{2}$/
+    end
+
+    # telegram validation
+    def self.valid_telegram?(telegram)
+        telegram.nil? || telegram =~ /@[a-zA-Z0-9_]{5,}$/
+    end
+
+    # git link validation
+    def self.valid_git?(git)
+        git.nil? || git =~ %r{^https?://github\.com/[a-zA-Z0-9_\-]+$}
+    end
+
+    # email validation
+    def self.valid_email?(email)
+        email.nil? || email =~ /^[\w+_.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    end
+
+    # names validation
+    def self.valid_name?(name)
+        name =~ /^[А-ЯЁ][а-яё]{1,}(-[А-ЯЁ][а-яё]{1,})?$/
+    end
 
     # checking for git availability
     def validate_git(git)
@@ -112,25 +120,7 @@ class Student
 
     # contacts validation
     def validate(git, phone_number, telegram, email)
-        validate_git(git)
-        validate_contacts(phone_number, telegram, email)
-    end
-
-    
-
-    # print info
-    def print_info
-        puts '-------------------'
-
-        puts "ID: #{ @id ? @id : 'Not Set' }"
-        puts "First Name: #{ @first_name }"
-        puts "Name: #{ @name }"
-        puts "Patronymic: #{ @patronymic }"
-        puts "Phone Number: #{ @phone_number ? @phone_number : 'Not Set' }"
-        puts "Telegram: #{ @telegram ? @telegram : 'Not Set' }"
-        puts "Email: #{ @email ? @email : 'Not Set' }"
-        puts "Git: #{ @git ? @git : 'Not Set' }"
-
-        puts '-------------------'
+        self.validate_git(git)
+        self.validate_contacts(phone_number, telegram, email)
     end
 end
