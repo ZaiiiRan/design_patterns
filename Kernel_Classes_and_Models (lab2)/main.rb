de8dc16c2c9e7d484dbf2411e18a946d3df5774a
ZaiiiRan <a1b075753@gmail.com>
2024-09-18 20:05:45 +0300

    testing the compatibility of methods for reading and writing to a file

--- Kernel_Classes_and_Models (lab2)/main.rb
+++ Kernel_Classes_and_Models (lab2)/main.rb
@@ -1,15 +1,12 @@
 require './student.rb'
 
-student1 = Student.new(id: 1, first_name: 'Смирнов', name: 'Никита', patronymic: 'Олегович', phone_number: '+7-(934)-453-32-11', 
-    telegram: '@zaiiran', git: 'https://github.com/ZaiiiRan')
-
-student2 = Student.new(first_name: 'Блягоз', name: 'Амаль', patronymic: 'Хазретович', id: 2, telegram: '@lamafout', git: 'https://github.com/lamafout',
-    email: 'lamafout@gmail.com')
-student3 = Student.new(first_name: 'Лотарев', name: 'Сергей', patronymic: 'Юрьевич', id: 3, telegram: '@lotarv', git: 'https://github.com/lotarv')
-student4 = Student.new_from_string("id:5, first_name: Иванов, name: Иван, patronymic: Иванович, telegram: @1vann, git: https://github.com/1vandfd")
-
 begin
-    Student.write_to_txt('./students.txt', [student1, student2, student3, student4])
+    students = Student.read_from_txt('./students.txt')
+    students.each do |x|
+        puts x.get_info
+    end
 rescue ArgumentError => e
     puts e.message
 end
+
+
