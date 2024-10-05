9429c351f12f0429f8e288479caf4ab36551b5bd
ZaiiiRan <a1b075753@gmail.com>
2024-10-05 09:07:15 +0300

    moving read from txt and write from txt functions to main file

--- Kernel_Classes_and_Models (lab2)/student.rb
+++ Kernel_Classes_and_Models (lab2)/student.rb
@@ -129,46 +129,6 @@ class Student < Person
         end
     end
 
-    # reading students from txt file
-    def self.read_from_txt(path)
-        if path.nil? || path.empty? then
-            raise ArgumentError, 'File path is nil or empty'
-        end
-        unless File.exist?(path) then
-            raise ArgumentError, 'File does not exists'
-        end
-
-        students = []
-        File.open(path, 'r') do |file|
-            file.each_line do |line|
-                begin
-                    student = self.new_from_string(line.strip)
-                    students << student
-                rescue ArgumentError => e
-                    raise ArgumentError, "#{line.strip} => #{e.message}"
-                end
-            end
-        end
-
-        students
-    end
-
-    #writing students in txt
-    def self.write_to_txt(path, students)
-        if path.nil? || path.empty? then
-            raise ArgumentError, 'File path is nil or empty'
-        end
-        if students.nil? || students.empty?
-            raise ArgumentError, 'Students list is empty or nil'
-        end
-
-        File.open(path, 'w') do |file|
-            students.each do |student|
-                file.puts student.to_line_s
-            end
-        end
-    end
-
     # to string
     def to_line_s
         data = []
