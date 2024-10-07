d552d5973ac89a4319a82f1dce7444ef7989d090
ZaiiiRan <a1b075753@gmail.com>
2024-10-07 13:32:45 +0300

    3.1.1: menu, input funcs and is_global_maximum

---
+++ polymorphism_blocks_collections_tests (lab3)/lists/main.rb
@@ -0,0 +1,91 @@
+require './processing.rb'
+
+def user_input
+    puts 'Вводите массив (все числа через пробел): '
+    arr = gets.chomp.split.map(&:to_i)
+end
+
+def file_input
+    puts 'Введите путь к файлу: '
+    path = gets.chomp
+
+    if path.nil? || path.empty? then
+        raise ArgumentError, 'Путь к файлу равен nil или пуст'
+    end
+    unless File.exist?(path) then
+        raise ArgumentError, 'Файл не существует'
+    end
+
+    arr = []
+    File.open(path, 'r') do |file|
+        arr = file.read.split(' ').map(&:to_i)
+    end
+
+    arr
+end
+
+def input_choice
+    arr = []
+    while true 
+        puts 'Выберите способ ввода данных: '
+        puts '1. С клавиатуры'
+        puts '2 - Из файла'
+    
+        choice = gets.chomp.to_i
+    
+        case choice
+        when 1
+            arr = user_input
+            break
+        when 2
+            arr = file_input
+            break
+        else
+            puts 'Нет такого способа'
+        end
+    end
+
+    puts "Введенный массив: #{arr.join(' ')}"
+    arr
+end
+
+
+def menu
+    while true
+        puts 'Выберите задачу:'
+        puts '1 - Проверить, является ли элемент глобальным максимумом'
+        puts '0 - Выход'
+    
+        task = gets.chomp.to_i
+    
+        case task
+        when 1
+            is_global_maximum
+        when 0
+            exit
+        else
+            puts 'Нет такой задачи'
+        end
+    end
+end
+
+def is_global_maximum
+    arr = input_choice
+
+    puts 'Введите индекс: '
+    index = gets.chomp.to_i
+
+    if index < 0 || index > arr.length - 1 then
+        puts 'Индекс некорректен'
+        return
+    end
+
+    if Processing.is_global_maximum(arr, index) then
+        puts "Элемент #{arr[index]} глобальный максимум"
+    else
+        puts "Элемент #{arr[index]} не глобальный максимум"
+    end
+end
+
+
+menu
---
+++ polymorphism_blocks_collections_tests (lab3)/lists/processing.rb
@@ -0,0 +1,5 @@
+class Processing
+    def self.is_global_maximum(arr, index)
+        return arr[index] == arr.max
+    end
+end
