d17ddf2fb01eab27c0a039dd202f2cfdfc5569f9
ZaiiiRan <a1b075753@gmail.com>
2024-11-06 00:34:14 +0300

    4.1.4: select, get selected, get names and get data methods

--- student/data_list.rb
+++ student/data_list.rb
@@ -2,10 +2,35 @@ class Data_list
     # constructor
     def initialize(elements)
         self.data = elements
+        self.selected = []
+    end
+
+    # select element id by number
+    def select(number)
+        element = self.data[number]
+        if element && !self.selected.include?(element.id)
+            self.selected << element.id
+        end
+    end
+
+    # get selected ids
+    def get_selected
+        self.selected.dup
+    end
+
+    # get names
+    def get_names
+        raise NotImplementedError, "Not implemented"
+    end
+
+    # get_data
+    def get_data
+        raise NotImplementedError, "Not implemented"
     end
 
     private
     attr_reader :data
+    attr_accessor :selected
 
     # data setter
     def data=(data)
